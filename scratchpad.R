6fc2afb4074e4958af1bdfeabf1751144d69f089
Eric Rynerson <eric.rynerson@gmail.com>
2016-11-22 11:50:03 -0800

    final effort

--- scratchpad.R
+++ scratchpad.R
@@ -1,5 +1,6 @@
 library(ggplot2)
 library(dplyr)
+library(grid)
 
 library(tibble)
 df <- state.x77 %>% data.frame %>% 
@@ -7,6 +8,11 @@ df <- state.x77 %>% data.frame %>%
   head(5) %>% 
   mutate(Illiteracy = ifelse(State == "Alabama", -Illiteracy, Illiteracy)) %>%
   mutate(Region = ifelse(State %in% c("Alabama", "Arkansas"), "South", "West"))
+
+ggname <- function(prefix, grob) {
+  grob$name <- grobName(grob, prefix)
+  grob
+}
 
 #' TODO:
 #' 1) [DONE] set positions
@@ -18,16 +24,31 @@ df <- state.x77 %>% data.frame %>%
 
 GeomBarMekko <- ggproto("GeomBarMekko", GeomBar,
                         setup_data = function(data, params) {
-                          w <- data$width # save for now
-                          pos <- 0.5 * (cumsum(w) + cumsum(c(0, w[-length(w)])))
+                          data$width <- data$width / sum(data$width) # TODO: need this?
+                          pos <- 0.5 * (cumsum(data$width) + cumsum(c(0, data$width[-length(data$width)])))
                           transform(data,
                                     ymin = pmin(y, 0), ymax = pmax(y, 0),
                                     xmin = pos - width / 2, xmax = pos + width / 2, width = NULL
                           )
                         },
-                        draw_panel = function(self, data, panel_scales, coord, width = NULL) {
-                          # Hack to ensure that width is detected as a parameter
-                          ggproto_parent(GeomRect, self)$draw_panel(data, panel_scales, coord)
+                        draw_panel = function(self, data, panel_scales, coord) {
+                          panel_scales$x.range <- c(0,1) # TODO: need this?
+                          coords <- coord$transform(data, panel_scales)
+                          browser()
+                            ggname("geom_rect", rectGrob(
+                              coords$xmin, coords$ymax,
+                              width = coords$xmax - coords$xmin,
+                              height = coords$ymax - coords$ymin,
+                              default.units = "native",
+                              just = c("left", "top"),
+                              gp = gpar(
+                                col = coords$colour,
+                                fill = alpha(coords$fill, coords$alpha),
+                                lwd = coords$size * .pt,
+                                lty = coords$linetype,
+                                lineend = "butt"
+                              )
+                            ))
                         }
 )
 
